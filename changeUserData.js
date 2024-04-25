4df4d312c271612fee8db9824250cc458b7af3ed
1308267@gmail.com <1308267@gmail.com>
2024-04-25 22:36:00 +0300

    feat: form for change user data added

---
+++ changeUserData.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    const changePasswordButton = $('#changePassword');
+    const saveChangedUserDataForm = $('#saveChangedUserData');
+
+    const userName = $('#userLoginForm').val();
+    const userPassword = $('#userPasswordForm').val();
+    const userFirstName = $('#userFirstNameForm').val();
+    const userLastName = $('#userLastNameForm').val();
+    const userPatronymicName = $('#userPatronymicName').val();
+})
